0f00c4ccaeaf038e9915e9b628a57816026d7121
sameerpathan07 <sameerpathan40694@gmail.com>
2026-09-19 12:04:00 +0530

    Remove unused movie booking code and fix test database

--- src/main/java/com/sameer/booking/repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sameer.booking.repository;
-
-import com.sameer.booking.entity.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking,Integer> {
-}
--- src/main/java/com/sameer/booking/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sameer.booking.repository;
-
-import com.sameer.booking.entity.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
-}
